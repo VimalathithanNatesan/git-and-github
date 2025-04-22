883959f072146b2a5c901aa79f92eea732881a5b
VimalathithanNatesan <vimal.py1989@gmail.com>
2025-04-22 14:04:12 +0530

    commended

--- git-github.py
+++ git-github.py
@@ -1,7 +1,7 @@
-"This is a git and github program information"
+'''"This is a git and github program information"
 Git:
 1.git is a version controal system, using of git we can able to change our version new to old or old to new
 2.using of git we can track our codes
 3.mutilple users can able to connect and work at the same time
 Git hub:
-git hub is web based repositary.
+git hub is web based repositary.'''
